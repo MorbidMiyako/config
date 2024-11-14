6c4b61e48b1622a95dc8819fbb253421fa6d71a4
Bernd de Kruik <bernd-de-kruik@hotmail.com>
2024-11-15 00:57:36 +0100

    prepull commit

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -189,3 +189,17 @@
        :config
        ;;literate
        (default +bindings +smartparens))
+
+(use-package pdf-tools
+  :defer t
+  ;; :commands (pdf-loader-install)
+  :mode "\\.pdf\\'"
+  :bind (:map pdf-view-mode-map
+              ("j" . pdf-view-next-line-or-next-page)
+              ("k" . pdf-view-previous-line-or-previous-page)
+              ("C-=" . pdf-view-enlarge)
+              ("C--" . pdf-view-shrink))
+  ;; :init (pdf-loader-install)
+  :config (add-to-list 'revert-without-query ".pdf"))
+
+(add-hook 'pdf-view-mode-hook #'(lambda () (interactive) (display-line-numbers-mode -1)))
