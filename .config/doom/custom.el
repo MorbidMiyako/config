fbab6e1f53aa21075aca92bd9a1ac7db7456659a
Bernd de Kruik <bernd-de-kruik@hotmail.com>
2024-11-29 15:04:33 +0100

    updates

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(pdf-tools)))
+ '(package-selected-packages '(lsp-javacomp java-imports lsp-java pdf-tools)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
